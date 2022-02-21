8f18461dee4c3569959a5b2079d78387d60bd22c
snowdenMoses <snowdenmoses@yahoo.com>
2022-02-21 13:45:02 +0100

    First Commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const mobileNav = document.querySelector('.mobile-nav')
 const navImage = document.querySelector('.nav-image')
-const navigation = document.querySelector('.navigation ul')
+const navigation = document.querySelector('.navigation')
 const navigation2 = document.querySelector('.navigation2')
 const stickyList = document.querySelector('.nav-list')
 const navList = document.querySelectorAll('#list')
@@ -25,9 +25,11 @@ const sliderId = document.getElementById('sliderId')
 
 
 
-window.addEventListener("load",()=>{
-    preloader.classList.add("hidden")
-})
+// // window.addEventListener("load",function(){
+//     preloader.classList.add("hidden")
+// })
+
+
 
 
 
@@ -36,12 +38,16 @@ mobileNav.addEventListener('click',()=>{
     let img = mobileNav.src;
     if (img.indexOf('menu-outline')!=-1) {
         navImage.src  = `images/close-outline.svg`
+
         stickyList.classList.toggle('nav-list_visible')
+        navigation.classList.toggle('navigation_newStyle')
+        
 
     }
     else {
                 navImage.src  = `images/menu-outline.svg`
                 stickyList.classList.toggle('nav-list_visible')
+                navigation.classList.toggle('navigation_newStyle')
     }
 
 
@@ -70,73 +76,6 @@ home.addEventListener('click',()=>{
     landingPageImage.scrollIntoView({behavior:"smooth"})
 
 })
-
-
-//Navbar change and become sticky after scrolling to section2
-// window.addEventListener('scroll',()=>{
-//     const sectionHeight = section2.clientHeight
-//     if(pageYOffset>section2.offsetTop){
-//         navigation.classList.add('navigation2')
-//     }
-//     else{
-
-//         navigation.classList.remove('navigation2')
-//     }
-    
-// })
-
-let x=0
-right.addEventListener("click",()=>{
-    if(x > "-300"){
-
-        x = x-100
-        // alert("Hello World")
-        sliders.style.right= x + "%"
-
-    }
-})
-
-left.addEventListener("click",()=>{
-    if(x < 0){
-
-        x = x+100
-        // alert("Hello World")
-        sliders.style.left= x + "%"
-
-    }
-})
-
-
-//Slider
-let images=[];
-let i = 0;
-// const slider0 = document.querySelector('.slider0')
-// const slider2 = document.querySelector('.slider2')
-
-images[0] = 'images/Coffee3.jpg'
-images[1] = 'images/Coffee4.jpg'
-images[2] = 'images/Coffee5.jpg'
-images[3] = 'images/Coffee6.jpg'
-
-
-
-const allSliders = document.querySelectorAll('.slider')
-
-
-const changeImageR = function(){
-    // slider.style.transition = "0.9s"
-    slider.src=images[i]
-    // slider.src=images[i+1]
-    // slider2.src=images[i+2]
-    if(i<images.length-1){
-        i++;
-    }
-    else{
-        i=0
-    }
-
-   setTimeout("changeImageR()",3000)
-}
 
 
 
